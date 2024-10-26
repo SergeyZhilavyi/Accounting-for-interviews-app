bb1ff1ed94d00a4e2da9e271872ae7214ce653b5
Sergey <64923733+SergeyZhilavyi@users.noreply.github.com>
2024-10-26 17:29:17 +0300

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,71 @@
+import './assets/main.css'
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+
+import { createApp } from 'vue'
+import { initializeApp } from "firebase/app"
+import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
+import ConfirmationService from 'primevue/confirmationservice'
+import { createPinia } from 'pinia'
+
+import Menubar from 'primevue/menubar'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import Toast from 'primevue/toast'
+import ProgressSpinner from 'primevue/progressspinner'
+import Card from 'primevue/card'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import ConfirmDialog from 'primevue/confirmdialog'
+import InlineMessage from 'primevue/inlinemessage'
+import InputNumber from 'primevue/inputnumber'
+import Textarea from 'primevue/textarea'
+import Calendar from 'primevue/calendar'
+import Radio from 'primevue/radiobutton'
+import Badge from 'primevue/badge'
+import Tooltip from 'primevue/tooltip'
+import Chart from 'primevue/chart'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDaz7lKATllOlMl4oODhMf_IJEKqB2UuJU",
+  authDomain: "inreviews-app.firebaseapp.com",
+  projectId: "inreviews-app",
+  storageBucket: "inreviews-app.appspot.com",
+  messagingSenderId: "612053826943",
+  appId: "1:612053826943:web:dd37382f2b0dad742bae4a"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig)
+
+app.use(ToastService)
+app.use(ConfirmationService)
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue)
+app.directive('tooltip', Tooltip)
+app.component('app-menubar', Menubar)
+app.component('app-button', Button)
+app.component('app-input-text', InputText)
+app.component('app-toast', Toast)
+app.component('app-progress', ProgressSpinner)
+app.component('app-card', Card)
+app.component('app-datatable', DataTable)
+app.component('app-column', Column)
+app.component('app-dialog', ConfirmDialog)
+app.component('app-message', InlineMessage)
+app.component('app-input-number', InputNumber)
+app.component('app-textarea', Textarea)
+app.component('app-calendar', Calendar)
+app.component('app-radio', Radio)
+app.component('app-badge', Badge)
+app.component('app-chart', Chart)
+
+app.mount('#app')
